3d1a29e7843f27e7d8b95f33257d8906d0f18981
KarthikCC27 <Karthikeyan_Karunanithi@comcast.com>
2024-01-31 08:50:33 +0530

    try resolving db error

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const con = require("C:\\comcast\\movie_app\\connect.js");
+const con = require("C:\\comcast\\movie_booking_nodeJS\\connect.js");
 
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const fs = require("fs");
-const html = fs.readFileSync("C:\\comcast\\movie_app\\main.html", "utf8");
+const html = fs.readFileSync("C:\\comcast\\movie_booking_nodeJS\\main.html", "utf8");
 const dom = new JSDOM(html);
 
 const document = dom.window.document;
@@ -11,11 +11,14 @@ const document = dom.window.document;
 con.query("select movieName from current_movies",function(err,results,fields){
     if (err) throw err;
 
-    const options = results.map((row)=>`<option>${row.movieName}</option>`).join('');
-    console.log(options);
     const myselect = document.getElementById("movie_name");
-    myselect.innerHTML=options;
+    for (let row of results) {
+      let option = document.createElement("option");
+      option.textContent = row.movieName;
+      myselect.appendChild(option);
+    }
 })
+
 
 
 
