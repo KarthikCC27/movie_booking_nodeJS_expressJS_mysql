0d2c4829e140f5bc5048b1c2d78ae73a87042ec6
KarthikCC27 <Karthikeyan_Karunanithi@comcast.com>
2024-02-09 19:40:05 +0530

    adding functionalities

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ const app = express();
 const con = require("./db")
 
 app.use(express.json());
+
+app.get("/index", (req, res) => {
+  res.sendFile("index.html",{root:__dirname});
+});
+
 app.get("/", (req, res) => {
-  res.sendFile("index.html", { root: __dirname });
+  res.sendFile("login.html", { root: __dirname });
+});
+
+app.get("/signup", (req, res) => {
+  res.sendFile("signup.html", { root: __dirname });
 });
 
 app.get("/showMovies", (req, res) => {
@@ -17,12 +26,34 @@ app.get("/showMovies", (req, res) => {
   });
 });
 
+app.post("/login",(req,res)=>{
+  const uname=req.body.login_data["user"].toString()
+  const pw=req.body.login_data["pass"].toString()
+  let query = `select * from users where username='${uname}'and pass='${pw}';`
+  con.query(query,(err,results)=>{
+      if (err) {
+          console.log(err)
+      }
+      else{
+        console.log(results.length)
+          if(results.length>0){
+            console.log("hi from if")
+            res.send({resp:"ok"})
+          }
+          else{
+            console.log("hi from else")
+            res.send({resp:"wrong_credentials"})
+          }
+      }
+  })
+})
+
 app.post("/bookTicket",(req,res)=>{
     const mname=req.body.movieticket["movie"].toString()
     const temp=req.body.movieticket["num"]
     let mticket=parseInt(temp);
 
-    let query = `insert into booked values(${mname},${mticket});`
+    let query = `insert into booked values('${mname}',${mticket});`
     con.query(query,(err)=>{
         if (err) {
             console.log(query)
