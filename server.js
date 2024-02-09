85fd6e75d496c684ec23676fc53c250b8d8d1154
KarthikCC27 <Karthikeyan_Karunanithi@comcast.com>
2024-02-09 21:13:44 +0530

    added login,signup

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const con = require("./db")
 
 app.use(express.json());
 
-app.get("/index", (req, res) => {
-  res.sendFile("index.html",{root:__dirname});
-});
-
 app.get("/", (req, res) => {
   res.sendFile("login.html", { root: __dirname });
+});
+
+app.get("/index", (req, res) => {
+  res.sendFile("index.html",{root:__dirname});
 });
 
 app.get("/signup", (req, res) => {
@@ -26,6 +26,33 @@ app.get("/showMovies", (req, res) => {
   });
 });
 
+app.post("/dosignup",(req,res)=>{
+  const uname=req.body.signup_data["user"].toString()
+  const pw=req.body.signup_data["pass"].toString()
+  let checkquery = `select * from users where username='${uname}';`
+  let query = `insert into users values('${uname}','${pw}');`
+  con.query(checkquery,(err,results)=>{
+      if (err) {
+          console.log(err)
+      }
+      else{
+          if(results.length==0){
+            con.query(query,(err)=>{
+              if(err) console.log(err);
+              else{
+                console.log("ok")
+                res.send({resp:"ok"})
+              }
+            })
+          }
+          else{
+            console.log("Exists")
+            res.send({resp:"exists"})
+          }
+      }
+  })
+})
+
 app.post("/login",(req,res)=>{
   const uname=req.body.login_data["user"].toString()
   const pw=req.body.login_data["pass"].toString()
@@ -35,13 +62,10 @@ app.post("/login",(req,res)=>{
           console.log(err)
       }
       else{
-        console.log(results.length)
-          if(results.length>0){
-            console.log("hi from if")
+          if(results.length!=0){
             res.send({resp:"ok"})
           }
           else{
-            console.log("hi from else")
             res.send({resp:"wrong_credentials"})
           }
       }
@@ -50,14 +74,14 @@ app.post("/login",(req,res)=>{
 
 app.post("/bookTicket",(req,res)=>{
     const mname=req.body.movieticket["movie"].toString()
+    const user=req.body.movieticket["uname"].toString()
     const temp=req.body.movieticket["num"]
     let mticket=parseInt(temp);
 
-    let query = `insert into booked values('${mname}',${mticket});`
+    let query = `insert into booked values('${mname}',${mticket},'${user}');`
     con.query(query,(err)=>{
         if (err) {
-            console.log(query)
-            console.log(typeof mname + " "+typeof mticket)
+            console.log(err)
         }
         else{
             console.log('ticket confirmed')
