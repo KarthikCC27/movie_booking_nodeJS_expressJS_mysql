3def2fa2c29244b1b181d2dfc25b3996d6182db3
KarthikCC27 <157583225+KarthikCC27@users.noreply.github.com>
2024-02-28 14:35:13 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql')
 const con = mysql.createConnection({
     host:"localhost",
     user:"root",
-    password:"K@rthik27122003",
-    database:"movies"
+    password:"Your_Password",
+    database:"Your_DB"
 })
 
 con.connect((err)=>{
